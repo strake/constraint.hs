c9c7bb6dce236446b959da9f945fd24b9fbbfe3a
M Farkas-Dyck <strake888@gmail.com>
2018-12-30 12:39:50 -0500

    Lifting

---
+++ Data/Constraint/Lifting.hs
@@ -0,0 +1,30 @@
+module Data.Constraint.Lifting where
+
+import Prelude hiding (Functor, (.), id)
+
+import Control.Categorical.Functor
+import Control.Category
+import Control.Category.Groupoid
+import Data.Constraint
+import Data.Functor.Classes
+import Data.Functor.Compose
+import Data.Morphism.Iso
+
+class Lifting c d f where
+    lift :: c a :- d (f a)
+
+type Endolifting c = Lifting c c
+
+--instance (∀ a . c a => d (f a)) => Lifting c d f where lift = Sub Dict
+
+instance Lifting Semigroup Monoid Maybe where lift = Sub Dict
+
+instance Lifting Semigroup Semigroup ((->) a) where lift = Sub Dict
+instance Lifting Monoid Monoid ((->) a) where lift = Sub Dict
+
+instance Semigroup a => Lifting Semigroup Semigroup ((,) a) where lift = Sub Dict
+instance Monoid a => Lifting Monoid Monoid ((,) a) where lift = Sub Dict
+
+instance Lifting (Functor s (->)) (Functor (NT s) (NT (->))) Compose where lift = Sub Dict
+
+instance Lifting Category Groupoid Iso where lift = Sub Dict
